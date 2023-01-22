f872146967abbe68aa8244c34a4cc6512cc3328a
Senuch <s.uzairtariq@gmail.com>
2023-01-22 14:50:16 +0500

    Added Jackson Configs

--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -7,8 +7,8 @@ public class CustomerFakeRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
         return Arrays.asList(
-                new Customer(1L, "Uzair Tariq"),
-                new Customer(2L, "Ali Raza")
+                new Customer(1L, "Uzair Tariq", "password"),
+                new Customer(2L, "Ali Raza", "password")
         );
     }
 }
